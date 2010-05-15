f4c1e469a94e0150efbd9b8788b8aff44e22f07d
Cameron <cheshirecam@yahoo.com>
2010-05-14 17:09:59 -0700

    took out requirements for gems

--- dvdogs/config/environment.rb
+++ dvdogs/config/environment.rb
@@ -39,7 +39,7 @@ Rails::Initializer.run do |config|
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
   
-  config.gem "tabs_on_rails", :source => "http://gemcutter.org" 
+  #config.gem "tabs_on_rails", :source => "http://gemcutter.org" 
 
-  config.gem "simple-navigation" 
+  #config.gem "simple-navigation" 
 end
