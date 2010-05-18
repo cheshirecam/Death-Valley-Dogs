f23985edd70046814bd7b1c5539ed314a7fdaaf1
Cameron <cheshirecam@yahoo.com>
2010-05-17 17:36:51 -0700

    New icon images

--- dvdogs/app/helpers/application_helper.rb
+++ dvdogs/app/helpers/application_helper.rb
@@ -1,26 +1,39 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def edit_icon
-    image_tag("edit-icon16.png", :border=>0)
+    image_tag("grunge_pencil.png", :border=>0)
   end
 
   def edit_icon_large
-    image_tag("edit-icon32.png", :border=>0)
+    image_tag("grunge_pencil.png", :border=>0)
   end
 
   def delete_icon
-    image_tag("delete-icon16.png", :border=>0)
+    image_tag("grunge_trash.png", :border=>0)
   end
   
   def delete_icon_large
-    image_tag("delete-icon32.png", :border=>0)
+    image_tag("grunge_trash.png", :border=>0)
   end
   
   def back_icon_large
-    image_tag("back-icon32.png", :border=>0)
+    image_tag("grunge_left_arrow.png", :border=>0)
+  end
+
+  def list_icon_large
+    image_tag("grunge_up_arrow.png", :border=>0)
   end
 
   def new_icon_large
-    image_tag("add-1-icon32.png", :border=>0)
+    image_tag("grunge_add.png", :border=>0)
   end
+  
+  def paws_icon
+    image_tag("grunge_paws.png", :border=>0)
+  end
+
+  def boots_icon
+    image_tag("grunge_boots.png", :border=>0)
+  end
+
 end
